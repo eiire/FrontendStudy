f3286de0c79161ff337d347db2e46c653e2f7793
Dmitriy <bulax.d.2101@gmail.com>
2020-07-20 20:54:46 +0700

    Repetition of prototypal inheritance

--- oop/clock/clock.js
+++ oop/clock/clock.js
@@ -20,10 +20,11 @@ class Clock {
         let secs = date.getSeconds();
         if (secs < 10) secs = '0' + secs;
 
+
         let output = template
             .replace('h', hours)
             .replace('m', mins)
-            .replace('s', secs);
+            .replace('s', secs)
 
         console.log(output);
     }
@@ -36,4 +37,62 @@ class Clock {
 test = new Clock({template: 'h:m:s'})
 Clock.render()
 test.start()
-// test.stop()
+test.stop()
+
+
+// task 2
+// console.log(ExtendedClock.prototype.__proto__) // === Clock.prototype
+
+class Clock2 {
+    constructor({ template }) {
+        this.template = template;
+    }
+
+    render() {
+        let date = new Date();
+
+        let hours = date.getHours();
+        if (hours < 10) hours = '0' + hours;
+
+        let mins = date.getMinutes();
+        if (mins < 10) mins = '0' + mins;
+
+        let secs = date.getSeconds();
+        if (secs < 10) secs = '0' + secs;
+
+        let output = this.template
+            .replace('h', hours)
+            .replace('m', mins)
+            .replace('s', secs);
+
+        console.log(output);
+    }
+
+    stop() {
+        clearInterval(this.timer);
+    }
+
+    start() {
+        this.render();
+        this.timer = setInterval(() => this.render(), 1000);
+    }
+}
+
+class ExtendedClock extends Clock2 {
+    constructor(template) {
+        super(template);
+        this.precision = template.precision || 1000
+    }
+    start() {
+        this.render();
+        // not closure
+        this.timer = setInterval(() => this.render(), this.precision);
+    }
+}
+
+let lowResolutionClock = new ExtendedClock({
+    template: 'h:m:s',
+    precision: 2000
+});
+
+lowResolutionClock.start();
